6094575c63535c3f9ceb3625bcc98427004e5880
Marjing <integration.admin@gaisanogroup.com>
2022-11-10 17:39:31 +0800

    resources/js/pages/bip/bip_index.js

--- resources/js/pages/bip/bip_index.js
+++ resources/js/pages/bip/bip_index.js
@@ -19,22 +19,14 @@ void new class BipIndex{
         document.querySelector('#upc_barcode').addEventListener('keydown',(e) => { 
             console.log(e.which)
             let upcData = (e.target.value == null ? '' : e.target.value)
-            //this.upcData = upcData
             if(e.which == 13){
-                this.upcData = this.passData()
+                this.upcData = upcData
                 console.log(upcData)
                 this.getItemBySku(upcData)
             }
 
         })
 
-    }
-
-    passData = ()=> {
-        let input2 = document.getElementById("upc_barcode").value;
-        $('#upc_dummy').val(input2)
-        let input3 = document.getElementById("upc_dummy").value;
-        return input3;
     }
 
     getItemBySku = async(barcode) => { 
