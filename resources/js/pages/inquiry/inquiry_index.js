1f3aab9c2af9d9aee3c1a7b8cf21a036f7c55d66
Marjing <integration.admin@gaisanogroup.com>
2023-04-04 14:10:07 +0800

    added sale price in web price inquiry

--- resources/js/pages/inquiry/inquiry_index.js
+++ resources/js/pages/inquiry/inquiry_index.js
@@ -7,11 +7,10 @@ void new class BipIndex{
     {
         this.initialization()
         this.eventHandler()
-        
     }
 
     initialization = () => { 
-        
+       
     }
     eventHandler = () => { 
       
@@ -19,11 +18,17 @@ void new class BipIndex{
             let upcData = (e.target.value == null ? '' : e.target.value)
             if(e.which == 13){
                 this.upcData = upcData
-                console.log(this.convertData(upcData))
+                console.log(this.convertData(upcData));
                 const current_date = document.querySelector('#current_date').value;
                 console.log(`current date : ${current_date}`); 
+
+                // const today = new Date();
+                // const year = today.getFullYear();
+                // const month = String(today.getMonth() + 1).padStart(2, '0');
+                // const day = String(today.getDate()).padStart(2, '0');
+                // const current_date = `${year}-${month}-${day}`;
+
                 this.getItemBySku(this.convertData(upcData),current_date);
-                 
             }
 
         })
@@ -41,31 +46,36 @@ void new class BipIndex{
           
          return addLeadingZeros
     }
-    
-    getItemBySku = async(barcode,current_date) => { 
+
+
+    getItemBySku = async(barcode,current_date) => {  
+        //const {data:result} = await axios.get(`/api/get/item/${barcode}`,{params:{code:this.currentCode.value}})
+        const sale_price = document.getElementById("sale_price");
+        const sale_term = document.getElementById("sale_term");
         try{
+            const {data:result} = await axios.get(`/api/get/item/${barcode}?current_date=${current_date}`);
             
-            const {data:result} = await axios.get(`/api/get/item/${barcode}?current_date=${current_date}`)
-            console.log(current_date);
             this.data = result    
             for(const data of result)
             {   
                 console.log(data.short_descr)      
                 console.log(`start : ${data.start_date}`)  
                 console.log(`end : ${data.stop_date}`)  
-                
                 $('#short_descr').html(data.short_descr)
-                $('#price').html('P'+ numberWithCommas(parseFloat(data.price).toFixed(2)))
                 $('#actual_barcode').html(data.upc)
+                
                 if(typeof data.start_date === 'undefined'){
-                    const price_before = document.getElementById("price_before");
-                    price_before.style.display = "none";
+                    sale_price.style.display = "none";
+                    sale_term.style.display = "none";
+                    $('#price').html('P'+ numberWithCommas(parseFloat(data.price).toFixed(2)))
                 }else{
                     const p_before = parseFloat(data.before);
                     const num = p_before.toFixed(2);
-                    const price_before = document.getElementById("price_before");
-                    price_before.style.display = "";
-                    $('#price_before').html(`BEFORE : PHP ${num}`)
+                    sale_price.style.display = "";
+                    sale_term.style.display = "";
+                    $('#price').html('P'+ numberWithCommas(num))
+                    $('#sale_term').html(`Sale Price : `);
+                    $('#sale_price').html(`PHP ${parseFloat(data.price).toFixed(2)}`);
                 }
                     
                 document.getElementById("upc_barcode").value = "";
@@ -77,6 +87,8 @@ void new class BipIndex{
             $('#price').html('--')
             $('#actual_barcode').html('--')
             document.getElementById("upc_barcode").value = "";
+            sale_price.style.display = "none";
+            sale_term.style.display = "none";
         }
     }
 }
