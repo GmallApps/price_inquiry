d82cb089bcf5048c5a041967631659c940b07abd
CristyMarjie <bragacristymarjie@gmail.com>
2023-07-12 16:46:17 +0800

    fixed auto slide bug

--- resources/views/pages/inquiry/inquiry_index.blade.php
+++ resources/views/pages/inquiry/inquiry_index.blade.php
@@ -89,7 +89,7 @@
 
 @push('scripts')
     <script src="{{ asset('/js/pages/inquiry/inquiry_index.js')}}"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 @endpush
 
